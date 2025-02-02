f3581057ce3022b0cd0aabf9869af1de781d04fd
Jason Waters <waters@adobe.com>
2025-02-02 11:26:10 -0700

    venmo link

--- src/venmo.js
+++ src/venmo.js
@@ -1,14 +1,17 @@
 const { getWorker } = require("./utils");
 
+function roundToCents(value) {
+  return parseFloat(value.toFixed(2));
+}
+
 function getVenmoLink(kimaiId, dollarAmount, { start, end }) {
   const worker = getWorker(kimaiId);
 
   if (!worker || !Object.hasOwnProperty.call(worker, "venmo")) {
     return undefined;
   }
-
   const note = `For pay period: ${start.format("MMM D")} to ${end.format("MMM D")}. 💰\n\nThank you!! ♥️️`;
-  return `https://venmo.com/?txn=pay&audience=private&recipients=${worker.venmo}&amount=${dollarAmount}&note=${note}`;
+  return `https://venmo.com/${worker.venmo}?txn=pay&audience=private&amount=${roundToCents(dollarAmount)}&note=${note}`;
 }
 
 module.exports = {
